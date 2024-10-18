b9dca261d89fff4daad23bf75e913ac7640ea0c8
keniiiiiii <kenidumb112@gmail.com>
2024-10-18 09:49:25 +0800

    Uploading files

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,16 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use Illuminate\Support\Facades\Mail;
+use App\Mail\ContactMe;
 
+// Home route
 Route::get('/', function () {
     return view('welcome');
+})->name('welcome');
+
+Route::post('/contact', function () {
+    $data = request()->all();
+    Mail::to('kaja2022-2721-41239@bicol-u.edu.ph')->send(new ContactMe($data));
+    return redirect('/contact')->with('flash', 'Message Sent Successfully');
 });
